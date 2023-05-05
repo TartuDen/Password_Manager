bcb386142a5069da673ee6457c38c91cdeb72af9
Pomogadryg <denver1033@gmail.com>
2023-05-05 16:57:31 +0300

    started

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+from tkinter import *
+import random
+
+new_window = Tk()
+new_window.minsize(width=400,height=300)
+new_window.config()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+new_window.mainloop()
+
+
+
+
+
+
+
+
+
 
